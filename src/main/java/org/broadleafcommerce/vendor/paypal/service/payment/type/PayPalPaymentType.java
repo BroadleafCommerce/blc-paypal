4096e08d673199f9f290ed0f7ea3e34f1d713892
Andre Azzolini <apazzolini@gmail.com>
2013-01-09 22:32:22 -0600

    Replace tabs with spaces

--- src/main/java/org/broadleafcommerce/vendor/paypal/service/payment/type/PayPalPaymentType.java
+++ src/main/java/org/broadleafcommerce/vendor/paypal/service/payment/type/PayPalPaymentType.java
@@ -49,7 +49,7 @@ public class PayPalPaymentType implements Serializable, BroadleafEnumerationType
     }
 
     public PayPalPaymentType(final String type, final String friendlyType) {
-    	this.friendlyType = friendlyType;
+        this.friendlyType = friendlyType;
         setType(type);
     }
 
@@ -58,10 +58,10 @@ public class PayPalPaymentType implements Serializable, BroadleafEnumerationType
     }
 
     public String getFriendlyType() {
-		return friendlyType;
-	}
+        return friendlyType;
+    }
 
-	private void setType(final String type) {
+    private void setType(final String type) {
         this.type = type;
         if (!TYPES.containsKey(type)) {
             TYPES.put(type, this);
