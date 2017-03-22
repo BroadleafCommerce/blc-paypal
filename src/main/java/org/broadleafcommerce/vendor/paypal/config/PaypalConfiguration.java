4642a200b5fe9ebe09fad07e51f9aad5f014da90
Nick Crum <ncrum@broadleafcommerce.com>
2017-03-22 12:13:41 -0500

    added in support for 5.2

---
+++ src/main/java/org/broadleafcommerce/vendor/paypal/config/PaypalConfiguration.java
@@ -0,0 +1,35 @@
+/*
+ * #%L
+ * BroadleafCommerce PayPal
+ * %%
+ * Copyright (C) 2009 - 2017 Broadleaf Commerce
+ * %%
+ * Licensed under the Broadleaf Fair Use License Agreement, Version 1.0
+ * (the "Fair Use License" located  at http://license.broadleafcommerce.org/fair_use_license-1.0.txt)
+ * unless the restrictions on use therein are violated and require payment to Broadleaf in which case
+ * the Broadleaf End User License Agreement (EULA), Version 1.1
+ * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt)
+ * shall apply.
+ * 
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
+ * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
+ * #L%
+ */
+package org.broadleafcommerce.vendor.paypal.config;
+
+import org.broadleafcommerce.common.logging.LifeCycleEvent;
+import org.broadleafcommerce.common.logging.ModuleLifecycleLoggingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Nick Crum ncrum
+ */
+@Configuration
+public class PaypalConfiguration {
+
+    @Bean
+    public ModuleLifecycleLoggingBean blEnterpriseModuleLifecycle() {
+        return new ModuleLifecycleLoggingBean(PaypalModuleRegistration.MODULE_NAME, LifeCycleEvent.LOADING);
+    }
+}
