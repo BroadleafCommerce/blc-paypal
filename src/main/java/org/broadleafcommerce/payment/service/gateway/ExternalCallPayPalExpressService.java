b37782ce84b00752cbd652986ac75f15bee543a3
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2018-08-31 22:30:20 -0500

    Added javadocs to the ExternalPayPalExpressService

--- src/main/java/org/broadleafcommerce/payment/service/gateway/ExternalCallPayPalExpressService.java
+++ src/main/java/org/broadleafcommerce/payment/service/gateway/ExternalCallPayPalExpressService.java
@@ -33,12 +33,41 @@ public interface ExternalCallPayPalExpressService {
 
     PayPalExpressConfiguration getConfiguration();
 
+    /**
+     * Converts a PayPal payment into a PaymentResponseDTO
+     * 
+     * @param response A PayPal payment that should be used to be converted into a PaymentResponseDTO
+     * @param responseDTO The response dto that should be used to copy information from the PayPal payment
+     */
     void setCommonDetailsResponse(Payment response, PaymentResponseDTO responseDTO);
 
+    /**
+     * Creates a PayPal payment
+     * 
+     * @param order The order the payment is being created for
+     * @param performCheckoutOnReturn Indicates if we should start checkout after the user has authorized the payment
+     * @return The new payment
+     * @throws PaymentException
+     */
     Payment createPayment(Order order, boolean performCheckoutOnReturn) throws PaymentException;
 
+    /**
+     * Updates the PayPal payment to be in sync with the order. This method should be used when fulfillment or pricing information changes
+     * after Payment creation. For creating payment use {@link #createPayment(Order, boolean)}
+     * 
+     * @param order The order that should be used to update the payment. The payment that updated is retrieved from the order's OrderPayments
+     * @return
+     * @throws PaymentException
+     */
     PayPalPaymentInfoDTO updatePaymentForFulfillment(Order order) throws PaymentException;
 
     PayPalResponse call(PayPalRequest paymentRequest) throws PaymentException;
+    /**
+     * Makes a request to PayPal
+     * 
+     * @param paymentRequest The payment request that should be executed. The operation that is executed is depedent on which implementation of {@link PayPalRequest} is sent
+     * @return the respective PayPalResponse that corresponds to the given PayPalRequest
+     * @throws PaymentException
+     */
 
 }
