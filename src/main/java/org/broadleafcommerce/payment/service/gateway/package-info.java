ea25b56f02f086c579fb78fbf62585cf04fc8542
Chris Kittrell <ckittrell@broadleafcommerce.com>
2019-09-30 13:30:53 -0500

    Introduce NonNullApi package-level annotations
    https://github.com/BroadleafCommerce/MicroPM/issues/1298

---
+++ src/main/java/org/broadleafcommerce/payment/service/gateway/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author Chris Kittrell (ckittrell)
+ */
+@NonNullApi
+package org.broadleafcommerce.payment.service.gateway;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/org/broadleafcommerce/vendor/paypal/service/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author Chris Kittrell (ckittrell)
+ */
+@NonNullApi
+package org.broadleafcommerce.vendor.paypal.service;
+
+import org.springframework.lang.NonNullApi;
