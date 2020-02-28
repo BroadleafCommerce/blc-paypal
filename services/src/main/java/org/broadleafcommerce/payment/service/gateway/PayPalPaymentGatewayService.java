b0578ac41abed416b7016d3f589d4b9b7de661d3
Dima Myroniuk <dmyroniuk@broadleafcommerce.com>
2020-02-28 10:59:51 +0200

    Remove unnecessary services. (BroadleafCommerce/CartOperationServices#173)

--- services/src/main/java/org/broadleafcommerce/payment/service/gateway/PayPalPaymentGatewayService.java
+++
@@ -1,8 +0,0 @@
-package org.broadleafcommerce.payment.service.gateway;
-
-import com.broadleafcommerce.paymentgateway.service.PaymentGatewayService;
-
-/**
- * @author Dima Myroniuk (dmyroniuk)
- */
-public interface PayPalPaymentGatewayService extends PaymentGatewayService {}
