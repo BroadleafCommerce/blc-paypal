23260d7a2c8babe6a29e76eff67105e3adbbc56f
jefffischer <jfischer@broadleafcommerce.org>
2020-04-20 20:32:43 -0500

    BroadleafCommerce/MicroPM#433 - Add license header

--- services/src/main/java/org/broadleafcommerce/vendor/paypal/service/autoconfigure/PayPalVendorServiceAutoConfiguration.java
+++ services/src/main/java/org/broadleafcommerce/vendor/paypal/service/autoconfigure/PayPalVendorServiceAutoConfiguration.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2009 - 2020 Broadleaf Commerce
+ *
+ * Licensed under the Broadleaf End User License Agreement (EULA), Version 1.1 (the
+ * "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt).
+ *
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the
+ * "Custom License") between you and Broadleaf Commerce. You may not use this file except in
+ * compliance with the applicable license.
+ *
+ * NOTICE: All information contained herein is, and remains the property of Broadleaf Commerce, LLC
+ * The intellectual and technical concepts contained herein are proprietary to Broadleaf Commerce,
+ * LLC and may be covered by U.S. and Foreign Patents, patents in process, and are protected by
+ * trade secret or copyright law. Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained from Broadleaf Commerce, LLC.
+ */
 package org.broadleafcommerce.vendor.paypal.service.autoconfigure;
 
 import org.broadleafcommerce.payment.service.gateway.PayPalCheckoutExternalCallService;
