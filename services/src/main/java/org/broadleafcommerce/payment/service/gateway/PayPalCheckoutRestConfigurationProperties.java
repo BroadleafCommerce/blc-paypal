f22ea96f440dd25e3979d58064d64c267b143069
jefffischer <jfischer@jfischer.com>
2023-09-15 10:48:21 -0500

    BroadleafCommerce/MicroPM#4046 - Update @PostConstruct package

--- services/src/main/java/org/broadleafcommerce/payment/service/gateway/PayPalCheckoutRestConfigurationProperties.java
+++ services/src/main/java/org/broadleafcommerce/payment/service/gateway/PayPalCheckoutRestConfigurationProperties.java
@@ -30,8 +30,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
-
+import jakarta.annotation.PostConstruct;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NonNull;
